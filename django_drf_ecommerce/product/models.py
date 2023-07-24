697fc18ee22fa649154acebffa45e5c3696f9557
Maxim Manzik <kepkpkep@gmail.com>
2023-07-24 16:42:19 +0300

    view Category, apiDoc, routers

--- django_drf_ecommerce/product/models.py
+++ django_drf_ecommerce/product/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
 
-class Category(models.Model):
+class Category(MPTTModel):
     name = models.CharField(max_length=255)
-    parent = TreeForeignKey('self', on_delete=models.PROTECT, null=True, blank=True)
+    parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
 
     class MPTTMeta:
         order_insertion_by = ['name']
@@ -25,7 +25,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     is_digital = models.BooleanField(default=False)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    category = TreeForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
+    category = TreeForeignKey("Category", on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.name
--- django_drf_ecommerce/product/views.py
+++ django_drf_ecommerce/product/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from drf_spectacular.utils import extend_schema
+
+from .models import Brand, Product, Category
+from .serializers import BrandSerializer, ProductSerializer, CategorySerializer
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for viewing categories
+    """
+
+    queryset = Category.objects.all()
+
+    @extend_schema(responses=CategorySerializer)
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
