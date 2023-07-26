2f5f2f30b0ea34036e26c688a9f9f19447960f7a
Maxim Manzik <kepkpkep@gmail.com>
2023-07-26 18:48:19 +0300

    Improve admin. Build filter category. Eliminating the N+1 query problem.

--- django_drf_ecommerce/product/models.py
+++ django_drf_ecommerce/product/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
 
+
 class Category(MPTTModel):
     name = models.CharField(max_length=255, unique=True)
     parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
@@ -22,10 +23,23 @@ class Brand(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
     description = models.TextField(blank=True)
     is_digital = models.BooleanField(default=False)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = TreeForeignKey("Category", on_delete=models.SET_NULL, null=True, blank=True)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+
+
+class ProductLine(models.Model):
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    sku = models.CharField(max_length=255)
+    stock_qty = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_line')
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.price)
--- django_drf_ecommerce/product/views.py
+++ django_drf_ecommerce/product/views.py
@@ -1,7 +1,14 @@
+from django.db import connection
+
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from drf_spectacular.utils import extend_schema
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import SqlLexer
+from sqlparse import format
 
 from .models import Brand, Product, Category
 from .serializers import BrandSerializer, ProductSerializer, CategorySerializer
@@ -27,6 +34,7 @@ class BrandViewSet(viewsets.ViewSet):
 
     queryset = Brand.objects.all()
 
+
     @extend_schema(responses=BrandSerializer)
     def list(self, request):
         serializer = BrandSerializer(self.queryset, many=True)
@@ -39,9 +47,36 @@ class ProductViewSet(viewsets.ViewSet):
     """
 
     queryset = Product.objects.all()
+    lookup_field = 'slug'
+
+    def retrieve(self, request, slug=None):
+        serializer = ProductSerializer(self.queryset.filter(slug=slug).select_related('category', 'brand'), many=True)
+        data = Response(serializer.data)
+
+        # q = list(connection.queries)
+        # print(len(q))
+        # for qs in q:
+        #     sqlformatted = format(str(qs['sql']), reindent=True)
+        #     print(highlight(sqlformatted, SqlLexer(), TerminalFormatter()))
+
+        return data
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data)
+
+    @action(methods=['GET'], detail=False, url_path=r"category/(?P<category>\w+)/all")
+    def list_product_by_category(self, request, category=None):
+        """
+        An endpoint to return products by category
+        """
+
+        serializer = ProductSerializer(self.queryset.filter(category__name=category), many=True)
+        return Response(serializer.data)
+
+
+
+
+
 
